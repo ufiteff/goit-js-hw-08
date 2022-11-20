371ee73c1ec381ee68e99aced9be9ff988f9e0f5
Hanna Andriushchenko <ufi.teff@gmail.com>
2022-11-20 11:36:24 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,4 +9,4 @@ function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') );
